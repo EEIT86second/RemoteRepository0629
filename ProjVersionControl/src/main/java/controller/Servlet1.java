c0b69e20ed702cd511b2a731982bb5a8d07cf550
江宗翰 <@EEIT>
2016-06-29 15:59:13 +0800

    將江江江

--- ProjVersionControl/src/main/java/controller/Servlet1.java
+++ ProjVersionControl/src/main/java/controller/Servlet1.java
@@ -29,14 +29,7 @@ public class Servlet1 extends HttpServlet {
 
 		
 		System.out.println("Servlet2");
-		System.out.println("Servlet3");
-		System.out.println("Servlet2");
-		System.out.println("Servlet3");
-		System.out.println("9+69+5Servlet1");
-		System.out.println("Servlet2");
-		System.out.println("Servlet3");
-		System.out.println("Servlet2");
-		System.out.println("Servlet3");
+	
 
 
 		HttpSession session = request.getSession();
