c444cc7d7ab8ca35b3556d254501e72b3e6b6f19
莊家搢 <@EEIT>
2016-06-29 16:38:42 +0800

    矽谷是我家

--- ProjVersionControl/src/main/java/controller/Servlet1.java
+++ ProjVersionControl/src/main/java/controller/Servlet1.java
@@ -16,7 +16,7 @@ public class Servlet1 extends HttpServlet {
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		
 
-		System.out.println("卡巴內瑞  !!!!!!!");
+		System.out.println("矽谷!!!");
 
 		HttpSession session = request.getSession();
 		session.setAttribute("time", ""+sDateFormat.format(new Date()));
